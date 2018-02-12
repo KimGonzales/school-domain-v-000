3a875162ec9f3c42e0e5a31b537e6d4dc13cc967
Kim Gonzales <kimgo589@gmail.com>
2018-02-12 20:12:44 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   end
 
   def add_student(student, grade)
-    roster[grade] = []
+    #roster[grade] = []
     if roster[grade].has_key?(grade)
       roster[grade] << student
     else
