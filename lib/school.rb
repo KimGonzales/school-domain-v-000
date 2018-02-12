33b945570e05496f0c2aaff1ee839debcbf2f109
Kim Gonzales <kimgo589@gmail.com>
2018-02-12 20:13:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
   def add_student(student, grade)
     #roster[grade] = []
-    if @roster[grade].has_key?(grade)
+    if @roster.has_key?(grade)
       @roster[grade] << student
     else
       @roster[grade] = [student]
